6d20c9a8601add53d71d9dae18fdd9411d2bfdaa
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-28 16:10:19 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,8 +3,26 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert("You notified the server!");
+      alert(serverMessage);
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
 
 
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
