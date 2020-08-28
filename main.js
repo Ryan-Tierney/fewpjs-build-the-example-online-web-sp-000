cf634bfc24ad37e7b860cac6472375057bbfda4a
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-28 16:12:44 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
 let colorStates = {
   "red" : "",
   "": "red"
 };
-// Your JavaScript code goes here!
-let articleHearts = document.querySelectorAll(".like-glyph");
+
+let articleHearts = document.querySelectorAll(".like");
 
 function likeCallback(e) {
   let heart = e.target;
-  mimicServerCall()
+  mimicServerCall("bogusUrl")
+   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
     .then(function(serverMessage){
-      alert("You notified the server!");
-      alert(serverMessage);
        heart.innerText = glyphStates[heart.innerText];
        heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
-      alert("Something went wrong!");
+      // Basic
+      // alert("Something went wrong!");
+      // or....
+      document.getElementById("modal").className = "";
     });
 }
-
 
 for (let glyph of articleHearts) {
   glyph.addEventListener("click", likeCallback);
